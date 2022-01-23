c287c91e0e1cf1532220c8c8d87dc0ecf3af3b5d
Vicente Mourão <vicente7mourao@gmail.com>
2022-01-23 01:34:35 +0000

    Let's goo

---
+++ src/components/Feed.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+import { feedQuery, searchQuery } from '../utils/data';
+
+const Feed = () => {
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null);
+  const { categoryId } = useParams();
+  useEffect(() => {
+    setLoading(true);
+    if (categoryId) {
+      const query = searchQuery(categoryId);
+
+      client.fetch(query).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+
+  if (loading) return <Spinner message='We are adding new ideas to your feed!' />;
+  if (!pins?.length) return <div className='flex justify-center font-bold items-center w-full text-xl mt-2'>No Pins Found!</div>;
+  return <div>{pins && <MasonryLayout pins={pins} />}</div>;
+};
+
+export default Feed;
---
+++ src/components/Login.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+import { useNavigate } from 'react-router-dom';
+import shareVideo from '../assets/share.mp4';
+import logo from '../assets/logowhite.png';
+import { client } from '../client';
+
+const Login = () => {
+  const navigate = useNavigate();
+  const responseGoogle = (response) => {
+    localStorage.setItem('user', JSON.stringify(response.profileObj));
+
+    const { name, googleId, imageUrl } = response.profileObj;
+
+    const doc = {
+      _id: googleId,
+      _type: 'user',
+      username: name,
+      image: imageUrl,
+    };
+
+    client.createIfNotExists(doc).then(() => {
+      navigate('/', { replace: true });
+    });
+  };
+
+  return (
+    <div className='flex justify-start items-center flex-col h-screen'>
+      <div className='relative w-full h-full'>
+        <video
+          src={shareVideo}
+          type='video/mp4'
+          loop={true}
+          controls={false}
+          muted={true}
+          autoPlay
+          className='w-full h-full object-cover'
+        />
+
+        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay'>
+          <div className='p-5'>
+            <img src={logo} alt='logo' width='130px' />
+          </div>
+
+          <div className='shadow-2xl'>
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy='single_host_origin'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
